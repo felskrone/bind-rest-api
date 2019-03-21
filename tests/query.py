78f518bdc3df34614cd8ce1d5ea055cd0a02b40d
Volker Schwicking <volker.schwicking@godaddy.com>
2019-03-21 12:56:30 +0000

    update

--- tests/query.py
+++ tests/query.py
@@ -10,18 +10,33 @@ import ast
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-api_url_int = 'http://127.0.0.1:9090/v0.0.1/{0}'
+api_add_int = 'http://127.0.0.1:9090/v0.0.1/{0}'
+api_del_int = 'http://127.0.0.1:9090/v0.0.1/{0}?{1}'
 
-def run(atype=None, data=None):
+def run(atype=None, data=None, operation='post'):
 
-    logger.info('Requesting interface-data from {0}'.format(api_url_int))
     headers = {'content-type': 'application/json'}
 
-    httpsreq = requests.post(
-        url=api_url_int.format(atype),
-        data=json.dumps(data),
-        headers=headers
-    )
+    logger.info('DELETEing record with URL {0}'.format(api_del_int.format(atype, 'arecord={0}'.format(data))))
+    if operation == 'post':
+        logger.info('POSTing data to URL {0}'.format(api_add_int.format(atype)))
+        httpsreq = requests.post(
+            url=api_add_int.format(atype),
+            data=json.dumps(data),
+            headers=headers
+        )
+
+    elif operation == 'delete':
+
+        url=api_del_int.format(atype, 'host=string&domain=hosteurope.de')
+
+        logger.info('DELETEing record with URL {0}'.format(url))
+
+        httpsreq = requests.delete(
+            url=url,
+            data=json.dumps(data),
+            headers=headers
+        )
 
     if httpsreq.status_code == 200:
         logger.info(pprint.pprint(ast.literal_eval(httpsreq.text)))
@@ -35,6 +50,9 @@ if __name__ == '__main__':
     A_multi = [
         {"domain": "hosteurope.de", "host": "string", "pointsTo": "198.51.100.42", "ttl": 0, "type":"A"},
         {"domain": "hosteurope.de", "host": "string", "pointsTo": "198.51.100.42", "ttl": 0}]
+
+
+    DELA_single = [{"domain": "hosteurope.de", "host": "string"}]
 
 
     TXT_single = [{"domain": "hosteurope.de", "host": "vs5", "data": "spf=xyz"}]
@@ -58,7 +76,8 @@ if __name__ == '__main__':
 #    run('MX', MX_single)
 #    run('MX', MX_single_type)
 #    run('MX', MX_multi)
-    run('A', A_single)
+#    run('A', A_single)
+    run('A', DELA_single, 'delete')
 #    run('A', A_single_type)
 #    run('A', A_multi)
 #    run('TXT', TXT_single)
