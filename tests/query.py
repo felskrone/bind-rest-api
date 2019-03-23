4fd537aff96c88822d386fb564a8d90c88a2c543
Volker Schwicking <volker.schwicking@godaddy.com>
2019-03-23 17:48:25 +0000

    fuer timo

--- tests/query.py
+++ tests/query.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import requests
 import sys
@@ -10,31 +10,26 @@ import ast
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-api_add_int = 'http://127.0.0.1:9090/v0.0.1/{0}'
-api_del_int = 'http://127.0.0.1:9090/v0.0.1/{0}?{1}'
+api_add_int = 'http://127.0.0.1:9090/v0.0.2/dns/{domain}/{rtype}/{host}'
 
-def run(atype=None, data=None, operation='post'):
+def run(method='post', domain=None, rtype=None, host=None, **options):
 
     headers = {'content-type': 'application/json'}
+    api_url = api_add_int.format(**{'domain': domain, 'rtype': rtype, 'host': host} )
+    logger.info(api_url)
+    logger.info(options)
 
-    logger.info('DELETEing record with URL {0}'.format(api_del_int.format(atype, 'arecord={0}'.format(data))))
-    if operation == 'post':
-        logger.info('POSTing data to URL {0}'.format(api_add_int.format(atype)))
+    if method == 'post':
         httpsreq = requests.post(
-            url=api_add_int.format(atype),
-            data=json.dumps(data),
+            url=api_url,
+            data=json.dumps(options.get('options')),
             headers=headers
         )
-
-    elif operation == 'delete':
-
-        url=api_del_int.format(atype, 'host=string&domain=hosteurope.de')
-
-        logger.info('DELETEing record with URL {0}'.format(url))
-
+    elif method == 'get':
+        pass
+    elif method == 'delete':
         httpsreq = requests.delete(
-            url=url,
-            data=json.dumps(data),
+            url=api_url,
             headers=headers
         )
 
@@ -45,43 +40,62 @@ def run(atype=None, data=None, operation='post'):
 
 if __name__ == '__main__':
 
-    A_single = [{"domain": "hosteurope.de", "host": "string", "pointsTo": "198.51.100.42", "ttl": 0}]
-    A_single_type = [{"domain": "hosteurope.de", "host": "string", "pointsTo": "198.51.100.42", "ttl": 0, "type":"A"}]
-    A_multi = [
-        {"domain": "hosteurope.de", "host": "string", "pointsTo": "198.51.100.42", "ttl": 0, "type":"A"},
-        {"domain": "hosteurope.de", "host": "string", "pointsTo": "198.51.100.42", "ttl": 0}]
+# A
+#    run(domain='hosteurope.de', rtype='A', host='vs6', options={"pointsTo": "198.51.100.42", "ttl": 100})
+#    input("Press Enter to continue...")
+#    run(method='delete', domain='hosteurope.de', rtype='A', host='vs6')
+#    input("Press Enter to continue...")
 
+# MX
+    run(domain='hosteurope.de', rtype='MX', host='mx666', options={"pointsTo": "1.2.3.4", "priority": 201})
+#    input("Press Enter to continue...")
+#    run(method='delete', domain='hosteurope.de', rtype='MX', host='vs6')
+#    input("Press Enter to continue...")
 
-    DELA_single = [{"domain": "hosteurope.de", "host": "string"}]
-
-
-    TXT_single = [{"domain": "hosteurope.de", "host": "vs5", "data": "spf=xyz"}]
-    TXT_single_cf = [{"domain": "hosteurope.de", "host": "vs5", "data": "spf=xyz", "txtConflictMatchingMode":"None", "txtConflictMatchingPrefix":"test"}]
-    TXT_single_type = [{"domain": "hosteurope.de", "host": "vs5", "data": "spf=xyz", "type":"TXT"}]
-    TXT_multi = [
-        {"domain": "hosteurope.de", "host": "vs5", "data": "spf=abc", "ttl": 0},
-        {"domain": "hosteurope.de", "host": "string", "data": "spf=cde", "ttl": 0}]
-
-    MX_single = [{"domain": "hosteurope.de", "host": "vs5", "pointsTo": "10.11.12.13", "ttl":100, "priority":100}]
-    MX_single_type = [{"domain": "hosteurope.de", "host": "vs5", "pointsTo": "10.11.12.13", "ttl":100, "priority":100, "type":"MX"}]
-    MX_multi = [
-        {"domain": "hosteurope.de", "host": "vs5", "pointsTo": "10.11.12.13", "ttl":100, "priority":100, "type":"MX"},
-        {"domain": "vs.de", "host": "fg5", "pointsTo": "10.15.52.53", "ttl":100, "priority":100, "type":"MX"}]
-
-    MULTI_multi = [
-        {"domain": "hosteurope.de", "host": "vs5", "data": "spf=abc", "ttl": 0, "type":"TXT"},
-        {"domain": "hosteurope.de", "host": "string", "pointsTo": "198.51.100.42", "ttl": 0, "type":"A"}
-    ]
-
-#    run('MX', MX_single)
-#    run('MX', MX_single_type)
-#    run('MX', MX_multi)
-#    run('A', A_single)
-    run('A', DELA_single, 'delete')
+#    input("Press Enter to continue...")
+#    run('A', DELA_single, 'delete')
+#    input("Press Enter to continue...")
 #    run('A', A_single_type)
+#    input("Press Enter to continue...")
 #    run('A', A_multi)
-#    run('TXT', TXT_single)
-#    run('TXT', TXT_single_cf)
-#    run('TXT', TXT_single_type)
-#    run('TXT', TXT_multi)
-#    run('MULTI', MULTI_multi)
+#
+    #    A_single_type = [{"domain": "hosteurope.de", "host": "string", "pointsTo": "198.51.100.42", "ttl": 0, "type":"A"}]
+#    A_multi = [
+#        {"domain": "hosteurope.de", "host": "Astring", "pointsTo": "198.51.100.42", "ttl": 0, "type":"A"},
+#        {"domain": "hosteurope.de", "host": "Bstring", "pointsTo": "198.51.100.43", "ttl": 0}]
+#    DELA_single = [{"domain": "hosteurope.de", "host": "string"}]
+#
+#
+#    TXT_single = [{"domain": "hosteurope.de", "host": "vs5", "data": "spf=xyz"}]
+#    TXT_single_cf = [{"domain": "hosteurope.de", "host": "vs5", "data": "spf=xyz", "txtConflictMatchingMode":"None", "txtConflictMatchingPrefix":"test"}]
+#    TXT_single_type = [{"domain": "hosteurope.de", "host": "vs5", "data": "spf=xyz", "type":"TXT"}]
+#    TXT_multi = [
+#        {"domain": "hosteurope.de", "host": "vs5", "data": "spf=abc", "ttl": 0},
+#        {"domain": "hosteurope.de", "host": "string", "data": "spf=cde", "ttl": 0}]
+#    DELTXT_single = [{"domain": "hosteurope.de", "host": "vs5", "data": "spf=xyz"}]
+#
+#    MX_single = [{"domain": "hosteurope.de", "host": "vs5", "pointsTo": "10.11.12.13", "ttl":100, "priority":100}]
+#    MX_single_type = [{"domain": "hosteurope.de", "host": "vs5", "pointsTo": "10.11.12.13", "ttl":100, "priority":100, "type":"MX"}]
+#    MX_multi = [
+#        {"domain": "hosteurope.de", "host": "vs5", "pointsTo": "10.11.12.13", "ttl":100, "priority":100, "type":"MX"},
+#        {"domain": "vs.de", "host": "fg5", "pointsTo": "10.15.52.53", "ttl":100, "priority":100, "type":"MX"}]
+#
+#    MULTI_multi = [
+#        {"domain": "hosteurope.de", "host": "vs5", "data": "spf=abc", "ttl": 0, "type":"TXT"},
+#        {"domain": "hosteurope.de", "host": "string", "pointsTo": "198.51.100.42", "ttl": 0, "type":"A"}
+#    ]
+#
+
+## MX
+#    run('MX', MX_single)
+##    run('MX', MX_single_type)
+##    run('MX', MX_multi)
+#
+## TXT
+##    run('TXT', TXT_single)
+##    run('TXT', TXT_single_cf)
+##    run('TXT', TXT_single_type)
+##    run('TXT', TXT_multi)
+#
+## MULTI
+##    run('MULTI', MULTI_multi)
