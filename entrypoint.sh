044733c408bec8ffc9230943f544801826fcc011
Marvin Dalheimer <me@marvin-dalheimer.de>
2019-03-24 15:11:53 +0100

    Made the watchmedo daemon look recursive

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 /etc/init.d/bind9 start
-watchmedo auto-restart -d "/app" -p "*.py;*.txt;*.yaml" python3 app.py
+watchmedo auto-restart -d "/app" --recursive -p "*.py;*.txt;*.yaml" python3 app.py
